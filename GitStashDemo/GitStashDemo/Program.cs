e3bdb67904e61677480230ba8b18493fbb4ef014
Sanjay Bhojwani <sbhojwani@infinniumtech.onmicrosoft.com>
2026-01-22 11:19:35 +0530

    Second commit

--- GitStashDemo/GitStashDemo/Program.cs
+++ GitStashDemo/GitStashDemo/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int a = 5;
+int b = 10;
+Console.WriteLine($"Sum of {a} and {b} is {a + b}");
+Console.WriteLine("This is a demo for Git stash functionality.");
